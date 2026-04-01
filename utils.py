869f358399cc24892b5a7e2722377757f955691f
Sujay Barai <sujaybarai46516@gmail.com>
2026-04-02 00:39:11 +0530

    Final clean repo structure

--- utils.py
+++
@@ -1,9 +0,0 @@
-import PyPDF2
-
-def extract_text_from_pdf(file):
-    pdf = PyPDF2.PdfReader(file.file)
-    text = ""
-    for page in pdf.pages:
-        if page.extract_text():
-            text += page.extract_text()
-    return text.lower()
